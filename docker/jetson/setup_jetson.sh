8e02bc6ec8f834b0953f1de2a59f7d16d7f824ab
Stash Pomichter <pomichterstash@gmail.com>
2025-04-08 14:51:11 -0700

    Jetson script change
    Former-commit-id: 97b62a0ad8725cb9c4ad9a745b73e2e42c8b7b4a

--- docker/jetson/setup_jetson.sh
+++ docker/jetson/setup_jetson.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Install cuSPARSELt 
+# Install cuSPARSELt
+wget https://developer.download.nvidia.com/compute/cusparselt/0.7.0/local_installers/cusparselt-local-tegra-repo-ubuntu2204-0.7.0_1.0-1_arm64.deb
 sudo dpkg -i cusparselt-local-tegra-repo-ubuntu2204-0.7.0_1.0-1_arm64.deb
 sudo cp /var/cusparselt-local-tegra-repo-ubuntu2204-0.7.0/cusparselt-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
