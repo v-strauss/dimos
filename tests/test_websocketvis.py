3dd26e8e2b4143dbd452ddb7b4106cccd1673e91
lesh <lesh@sysphere.org>
2025-04-22 10:22:47 -0700

    websocket vis test
    Former-commit-id: ae65e6349707375f1cb56d20ff68776ba65b6e73

--- tests/test_websocketvis.py
+++ tests/test_websocketvis.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python3
-
+import time
 from dimos.web.websocket_vis.server import WebsocketVis
+from dimos.robot.global_planner.planner import AstarPlanner
+from dimos.robot.global_planner.costmap import Costmap
+from dimos.robot.global_planner.vector import Vector
 
 
 def main():
     # Start the WebSocket server
-    websocket_vis = WebsocketVis(use_reload=True)
+    websocket_vis = WebsocketVis()
     websocket_vis.start()
+
+    planner = AstarPlanner(
+        costmap=lambda: Costmap.from_pickle(f"{__file__.rsplit('/', 1)[0]}/mockdata/costmap.pickle"),
+        base_link=lambda: [Vector(1, 1, 0), Vector(1, 1, 1)],
+        local_nav=lambda x: time.sleep(1) and True,
+    )
+
+    websocket_vis.connect(planner.vis_stream())
+
+    time.sleep(1)  # Allow time for the server to start
+    planner.plan(Vector([3, 4, 0]))
 
     print(f"WebSocket server started on port {websocket_vis.port}")
     try:
