e74d8a81ad291ab32fcd8704e9aad6fab794ccd2
stash <pomichterstash@gmail.com>
2025-04-22 22:10:04 -0700

    import fix
    Former-commit-id: f1d1d953e676509f8d33d5f5b29df87a94fa7c46

--- tests/test_claude_agent_skills_query.py
+++ tests/test_claude_agent_skills_query.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
 import tests.test_header
+import os
 
 import time
 from dotenv import load_dotenv
