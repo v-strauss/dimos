e509b1d5cff0d21ef0cf2317e82e195876a082ac
Matthew Daiter <mdaiter8121@gmail.com>
2025-09-08 22:43:24 +0000

    Add default capacities in unitree_go2.py
    Former-commit-id: c2fbe9f6f65506ced6c672dfa8c2ed7f9a5a85b9

--- dimos/robot/unitree_webrtc/unitree_go2.py
+++ dimos/robot/unitree_webrtc/unitree_go2.py
@@ -78,6 +78,17 @@ logging.getLogger("root").setLevel(logging.WARNING)
 warnings.filterwarnings("ignore", message="coroutine.*was never awaited")
 warnings.filterwarnings("ignore", message="H264Decoder.*failed to decode")
 
+"""
+Constants for shared memory
+Usually, auto-detection for size would be preferred. Sadly, though, channels are made
+and frozen *before* the first frame is received.
+Therefore, a maximum capacity for color image and depth image transfer should be defined
+ahead of time.
+"""
+# Default color image size: 1920x1080 frame x 3 (RGB) x uint8
+DEFAULT_CAPACITY_COLOR_IMAGE = 1920 * 1080 * 3
+# Default depth image size: 1280x720 frame * 4 (float32 size)
+DEFAULT_CAPACITY_DEPTH_IMAGE = 1280 * 720 * 4
 
 class FakeRTC:
     """Fake WebRTC connection for testing with recorded data."""
@@ -389,7 +400,7 @@ class UnitreeGo2(Robot):
 
         self.connection.lidar.transport = core.LCMTransport("/lidar", LidarMessage)
         self.connection.odom.transport = core.LCMTransport("/odom", PoseStamped)
-        self.connection.video.transport = core.pSHMTransport("/go2/color_image")
+        self.connection.video.transport = core.pSHMTransport("/go2/color_image", default_capacity=DEFAULT_CAPACITY_COLOR_IMAGE)
         self.connection.movecmd.transport = core.LCMTransport("/cmd_vel", Twist)
         self.connection.camera_info.transport = core.LCMTransport("/go2/camera_info", CameraInfo)
         self.connection.camera_pose.transport = core.LCMTransport("/go2/camera_pose", PoseStamped)
@@ -477,7 +488,8 @@ class UnitreeGo2(Robot):
             output_dir=self.spatial_memory_dir,
         )
 
-        self.spatial_memory_module.video.transport = core.pSHMTransport("/go2/color_image")
+        color_image_default_capacity = 1920 * 1080 * 4
+        self.spatial_memory_module.video.transport = core.pSHMTransport("/go2/color_image", default_capacity=DEFAULT_CAPACITY_COLOR_IMAGE)
         self.spatial_memory_module.odom.transport = core.LCMTransport(
             "/go2/camera_pose", PoseStamped
         )
@@ -509,8 +521,8 @@ class UnitreeGo2(Robot):
         self.depth_module = self.dimos.deploy(DepthModule, gt_depth_scale=gt_depth_scale)
 
         # Set up transports
-        self.depth_module.color_image.transport = core.pSHMTransport("/go2/color_image")
-        self.depth_module.depth_image.transport = core.pSHMTransport("/go2/depth_image")
+        self.depth_module.color_image.transport = core.pSHMTransport("/go2/color_image", default_capacity=DEFAULT_CAPACITY_COLOR_IMAGE)
+        self.depth_module.depth_image.transport = core.pSHMTransport("/go2/depth_image", default_capacity=DEFAULT_CAPACITY_DEPTH_IMAGE)
         self.depth_module.camera_info.transport = core.LCMTransport("/go2/camera_info", CameraInfo)
 
         logger.info("Camera module deployed and connected")
