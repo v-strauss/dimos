d69a4609b231a6d710d505f1c337df734586d9b3
lesh <lesh@sysphere.org>
2025-10-24 19:01:09 +0300

    spec files
    Former-commit-id: 2e98745d3d1fcc8fb2e7cb97ea0e6d245ae36f0f

---
+++ dimos/spec/map.py
@@ -0,0 +1,31 @@
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Protocol
+
+from dimos.core import Out
+from dimos.msgs.nav_msgs import OccupancyGrid
+from dimos.msgs.sensor_msgs import PointCloud2
+
+
+class Global3DMap(Protocol):
+    global_pointcloud: Out[PointCloud2]
+
+
+class GlobalMap(Protocol):
+    global_map: Out[OccupancyGrid]
+
+
+class GlobalCostmap(Protocol):
+    global_costmap: Out[OccupancyGrid]
