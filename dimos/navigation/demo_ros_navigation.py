34e30512a48e9b923184f5b20aaf461b7beac8ae
Paul Nechifor <paul@nechifor.net>
2025-10-30 22:34:43 +0200

    use ROS_DOMAIN_ID=42
    Former-commit-id: 5d5bc0f17f3e428d8a4e7f699b2b136f62f0d050

--- dimos/navigation/demo_ros_navigation.py
+++ dimos/navigation/demo_ros_navigation.py
@@ -1,6 +1,20 @@
-import time
-import rclpy
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import time
+
+import rclpy
 
 from dimos import core
 from dimos.msgs.geometry_msgs import PoseStamped, Quaternion, Twist, Vector3
@@ -9,7 +23,6 @@ from dimos.msgs.sensor_msgs import PointCloud2
 from dimos.navigation.rosnav import ROSNav
 from dimos.protocol import pubsub
 from dimos.utils.logging_config import setup_logger
-
 
 logger = setup_logger(__file__)
 
