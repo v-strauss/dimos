4b91e238719ce96181a0522fb69cd58cfaa41201
lesh <lesh@sysphere.org>
2025-12-21 07:16:43 +0000

    nav/pointcloud spec proposal
    Former-commit-id: 61be09aaecf268297102f17253cba304e051d034 [formerly 0cf1113851788ba8c80a1ac05f6b2c8b5f34b887] [formerly eece386a0f8c72f458db960f806550035531f312 [formerly f8bef48cf85f129b87d4be7380d9474afce6ff71]]
    Former-commit-id: 27ab913eca8e2e29bc07e88a341474b73ea2ccac
    Former-commit-id: 73b5c38d70d87af853221c6eb4f5a22c2e1e0923

---
+++ dimos/navigation/spec.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC
+
+from dimos.core import In, Out
+from dimos.msgs.geometry_msgs import Path, PoseStamped, Twist
+
+
+class NavSpec(ABC):
+    goal_req: In[PoseStamped] = None  # type: ignore
+    goal_active: Out[PoseStamped] = None  # type: ignore
+    path_active: Out[Path] = None  # type: ignore
+    ctrl: Out[Twist] = None  # type: ignore
+
+    # identity quaternion (Quaternion(0,0,0,1)) represents "no rotation requested"
+    def goto(self, target: PoseStamped) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
