d14db6d6e139e7ba0aaf607e4c4ce4237f9137f9
mustafab0 <mustafabhadsorawala@gmail.com>
2025-12-25 10:02:24 +0000

    restructured hardware folder
    Former-commit-id: 2ed331f6bc8c1fa516c597e8f55214f7f762fa21 [formerly 9175af6dd03245d5b1a301253a77ed2410ef3c1c]
    Former-commit-id: 4f0eb92e25925a7b9aaf9332875417cb558f169b

--- dimos/manipulation/visual_servoing/manipulation_module.py
+++ dimos/manipulation/visual_servoing/manipulation_module.py
@@ -28,8 +28,7 @@ from dimos_lcm.sensor_msgs import CameraInfo  # type: ignore[import-untyped]
 import numpy as np
 from reactivex.disposable import Disposable
 
-from dimos.core import In, Module, Out, rpc
-from dimos.hardware.piper_arm import PiperArm
+from dimos.hardware.manipulators.piper.piper_arm import PiperArm
 from dimos.manipulation.visual_servoing.detection3d import Detection3DProcessor
 from dimos.manipulation.visual_servoing.pbvs import PBVS
 from dimos.manipulation.visual_servoing.utils import (
