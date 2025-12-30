cad425dd15e0e518e528c97b714b7e37b583e879
Paul Nechifor <paul@nechifor.net>
2025-12-31 01:59:18 +0200

    remove deprecated docker files (#912)
    Former-commit-id: 394bd9e32f8302d781417b61126bf41b91c5754b

--- docker/deprecated/simulation/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYTHONPATH="${PYTHONPATH}:/app"
-source /opt/ros/humble/setup.bash
-#source /home/ros/dev_ws/install/setup.bash
-exec "$@" 
--- docker/deprecated/unitree/ros/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# Source ROS2 environment
-source /opt/ros/${ROS_DISTRO}/setup.bash
-source /ros2_ws/install/setup.bash
-# Execute the command passed to docker run
-exec "$@"
